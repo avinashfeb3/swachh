7b863de8c1cfa3a175100aef6f4977ee316537e1
“avinash.webintern@gmail.com” <avinash.webintern@gmail.com>
2024-05-13 17:43:58 +0530

    New files created for the below page

---
+++ about.php
@@ -0,0 +1,13 @@
+<!-- Header Include -->
+<?php
+    include 'header.php';
+?>
+
+<body>
+
+</body>
+
+<!-- Footer Include -->
+<?php
+  include 'footer.php';
+?>
---
+++ gallery.php
@@ -0,0 +1,13 @@
+<!-- Header Include -->
+<?php
+    include 'header.php';
+?>
+
+<body>
+    <h1>Gallery section</h1>
+</body>
+
+<!-- Footer Include -->
+<?php
+  include 'footer.php';
+?>
