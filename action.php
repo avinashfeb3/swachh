ebf4b324da28ff763d950f781a953e2351c0bfd6
avinash.webintern@gmail.com <avinash.webintern@gmail.com>
2024-05-14 16:39:35 +0530

    created all the pages.

--- action.php
+++ action.php
@@ -1,13 +1,54 @@
 <!-- Header Include -->
 <?php
-    include 'header.php';
+include 'header.php';
 ?>
 
 <body>
+  <!-- Swachh Action Banner Section Start -->
+  <section class="about_wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 mb-4 mb-md-0 align-items-center justify-content-center">
+          <h2 className="about_text">Swachh Action Plan</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Swachh Action Banner Section End -->
 
+  <!-- Table os Swachh Action Plan Start -->
+  <section class="table_wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="card mt-5 pt-5 mb-5 pb-5">
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-bordered">
+                <thead>
+                  <tr class=" table-primary">
+                    <th scope="col">S.No</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Download</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Table os Swachh Action Plan End -->
 </body>
 
 <!-- Footer Include -->
 <?php
-  include 'footer.php';
+include 'footer.php';
 ?>
--- gallery.php
+++ gallery.php
@@ -1,13 +1,111 @@
 <!-- Header Include -->
 <?php
-    include 'header.php';
+include 'header.php';
 ?>
 
 <body>
-    <h1>Gallery section</h1>
+  <!-- Gallery Banner Section Start -->
+  <section class="about_wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 mb-4 mb-md-0 align-items-center justify-content-center">
+          <h2 className="about_text">Gallery</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Gallery Banner Section End -->
+
+  <!-- Main Gallery Section Start -->
+  <section class="main_wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="card">
+          <div class="card-body">
+            <div class="row mx-3 mt-3 mb-2 pt-3 pb-2">
+              <div class="col-md-3">
+                <form>
+                  <div class="form-floating">
+                    <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                      <option selected>Please Select Organization</option>
+                      <option value="1">STQC</option>
+                      <option value="2">Bisag</option>
+                      <option value="3">Meity</option>
+                    </select>
+                    <label for="floatingSelect">Organization</label>
+                  </div>
+                </form>
+              </div>
+              <div class="col-md-3">
+                <form>
+                  <div class="form-floating mb-3">
+                    <input type="date" class="form-control" id="floatingInput" placeholder="Select Start Date..">
+                    <label for="floatingInput">Start Date</label>
+                  </div>
+                </form>
+              </div>
+              <div class="col-md-3">
+                <form>
+                  <div class="form-floating mb-3">
+                    <input type="date" class="form-control" id="floatingInput" placeholder="Select End Date..">
+                    <label for="floatingInput">End Date</label>
+                  </div>
+                </form>
+              </div>
+              <div class="col-md-3">
+                <a href="#">
+                  <button type="submit" class="btn view_btn">Apply</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Main Gallery Section End -->
+
+  <!-- Gallery Item Showing Section Start -->
+  <section class="main_wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <img src="https://dummyimage.com/100x100/cccccc/fff" class="card-img-top" alt="Image 1">
+            <div class="card-body">
+              <h5 class="card-title">STQC</h5>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <img src="https://dummyimage.com/100x100/cccccc/fff" class="card-img-top" alt="Image 2">
+            <div class="card-body">
+              <h5 class="card-title">Meity</h5>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <img src="https://dummyimage.com/100x100/cccccc/fff" class="card-img-top" alt="Image 3">
+            <div class="card-body">
+              <h5 class="card-title">Bisag</h5>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Gallery Item Showing Section End -->
 </body>
 
 <!-- Footer Include -->
 <?php
-  include 'footer.php';
+include 'footer.php';
 ?>
